3238587b6d9e50698814f94671b4bfda90ffa19e
jjwernerm <joannywerner@gmail.com>
2024-08-26 14:19:35 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './layout/Header.jsx';
+import Footer from './layout/Footer.jsx';
+import DashboardMern from './views/DashboardMern.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <DashboardMern />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <>    
+      <div className='flex justify-center p-10 bg-gradient-to-r from-gray-500 to-gray-800 w-screen'>
+        <h1 className='text-2xl font-bold text-white'>Dashboard</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
